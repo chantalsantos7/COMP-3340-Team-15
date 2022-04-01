5df1b616fb284a8b25681519a41f1155bc22d44a
filbeyr <100979098+filbeyr@users.noreply.github.com>
2022-04-01 13:09:06 -0400

    Add files via upload

---
+++ navCartImage.js
@@ -0,0 +1,9 @@
+function navCartImage(){
+  var myPhpValue = $("#myPhpValue").val();
+  if(myPhpValue == 2 || myPhpValue == 3){
+    window.location.href = "Cart.php";
+  }
+  else{
+    window.location.href = "LoginPage.php";
+  }
+}
