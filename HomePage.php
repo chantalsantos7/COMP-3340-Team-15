170c1a744caa337baddb06125ecd688e51ea0e1d
filbeyr <100979098+filbeyr@users.noreply.github.com>
2022-04-06 12:46:45 -0400

    removed broken footer links and linked sitemap

--- HomePage.php
+++ HomePage.php
@@ -129,27 +129,12 @@ $conn->close();
         <div class="footer-links">
             <ul class="category">
                 <li class="category-name">men</li>
-                <li><a href="#" class="footer-link">brand</a></li>
-                <li><a href="#" class="footer-link">brand</a></li>
-                <li><a href="#" class="footer-link">brand</a></li>
-                <li><a href="#" class="footer-link">brand</a></li>
-                <li><a href="#" class="footer-link">brand</a></li>
             </ul>
             <ul class="category">
                 <li class="category-name">women</li>
-                <li><a href="#" class="footer-link">brand</a></li>
-                <li><a href="#" class="footer-link">brand</a></li>
-                <li><a href="#" class="footer-link">brand</a></li>
-                <li><a href="#" class="footer-link">brand</a></li>
-                <li><a href="#" class="footer-link">brand</a></li>
           	</ul>
           <ul class="category">
                 <li class="category-name">kids</li>
-                <li><a href="#" class="footer-link">brand</a></li>
-                <li><a href="#" class="footer-link">brand</a></li>
-                <li><a href="#" class="footer-link">brand</a></li>
-                <li><a href="#" class="footer-link">brand</a></li>
-                <li><a href="#" class="footer-link">brand</a></li>
             </ul>
         </div>
     </div>
@@ -160,7 +145,7 @@ $conn->close();
     <p class="info">phone: </p>
     <div class="footer-other-links">
         <div>
-            <a href="#" class="sitemap-link">sitemap</a>
+            <a href="sitemap.html" class="sitemap-link">sitemap</a>
         </div>
     </div>
     <p class="footer-ending">Shoaza online store</p>
